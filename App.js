482e71c7a957ff30349196013d1a73b66fbe31f4
Nik Tomazic <nick.tomazic@gmail.com>
2023-07-21 22:36:33 +0200

    fetching data from back4app

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from "react";
+import MapView, {Marker} from "react-native-maps";
+import {StyleSheet, View} from "react-native";
+
+import Parse from "parse/react-native.js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+Parse.setAsyncStorage(AsyncStorage);
+Parse.initialize(process.env.EXPO_PUBLIC_PARSE_APPLICATION_ID, process.env.EXPO_PUBLIC_PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = "https://parseapi.back4app.com/";
+
+const initialRegion = {
+  latitude: 37.0000,
+  longitude: -100.0000,
+  latitudeDelta: 64,
+  longitudeDelta: 64,
+};
 
 export default function App() {
+
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    const Place = Parse.Object.extend("Place");
+    const placeQuery = new Parse.Query(Place);
+    placeQuery.find()
+      .then((places) => {
+        places = places.map((place) => {
+          return {
+            title: place.get("name"),
+            description: place.get("description"),
+            coordinate: {
+              latitude: place.get("location").latitude,
+              longitude: place.get("location").longitude,
+            },
+          };
+        });
+        setPlaces(places);
+      })
+      .catch((error) => {
+        console.error("Error retrieving places: ", error);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        initialRegion={initialRegion}
+        style={styles.map}
+      >
+        {places.map((place, index) => (
+          <Marker
+            key={index}
+            title={place.title}
+            description={place.description}
+            coordinate={place.coordinate}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
@@ -13,8 +64,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
